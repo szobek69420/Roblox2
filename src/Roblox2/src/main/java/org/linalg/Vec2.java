b874651f8cd4a00fa9fc8b2c9d0b378a71b29b66
Szobek Ádám Márk <szobek.adam@gmail.com>
2024-03-02 17:11:43 +0100

    yee

--- src/Roblox2/src/main/java/org/linalg/Vec2.java
+++ src/Roblox2/src/main/java/org/linalg/Vec2.java
@@ -9,10 +9,10 @@ public class Vec2 implements Cloneable{
         this.y=y;
     }
 
-    public Vec2(float szam)
+    public Vec2(float nummah)
     {
-        this.x=szam;
-        this.y=szam;
+        this.x=nummah;
+        this.y=nummah;
     }
 
     public Vec2()
@@ -21,19 +21,75 @@ public class Vec2 implements Cloneable{
         this.y=0;
     }
 
-    @Override
-    public Vec2 clone() {
-        try {
-            Vec2 clone = (Vec2) super.clone();
-            // TODO: copy mutable state here, so the clone can't change the internals of the original
-            return clone;
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
+    public void scale(float factor)
+    {
+        x*=factor;
+        y*=factor;
+    }
+
+    public float sqrMagnitude()
+    {
+        return x*x+y*y;
+    }
+
+    public float magnitude()
+    {
+        return (float)Math.sqrt(x*x+y*y);
+    }
+
+    public void normalize()
+    {
+        float length=magnitude();
+        if(length>0.00001f)
+        {
+            x/=length;
+            y/=length;
         }
     }
 
+    @Override
+    public Vec2 clone() {
+        Vec2 cloned=new Vec2(this.x, this.y);
+        return cloned;
+    }
+
+    //static stuff
+    public static Vec2 up=new Vec2(0,1);
+    public static Vec2 down=new Vec2(0, -1);
+    public static Vec2 left=new Vec2(-1, 0);
+    public static Vec2 right=new Vec2(1,0);
+
     public static Vec2 sum(Vec2 a, Vec2 b)
     {
-        return new Vec2(0);
+        return new Vec2(a.x+b.x, a.y+b.y);
+    }
+
+    public static Vec2 subtract(Vec2 a, Vec2 b)
+    {
+        return new Vec2(a.x-b.x, a.y-b.y);
+    }
+
+    public float dot(Vec2 a, Vec2 b)
+    {
+        return a.x*b.x+a.y*b.y;
+    }
+
+    public static Vec2 scale(Vec2 vec, float factor)
+    {
+        Vec2 temp=vec.clone();
+        temp.scale(factor);
+        return temp;
+    }
+
+    public static Vec2 normalize(Vec2 vec)
+    {
+        Vec2 temp=vec.clone();
+        temp.normalize();
+        return temp;
+    }
+
+    public static Vec2 lerp(Vec2 a, Vec2 b, float i)
+    {
+        return new Vec2(a.x+(b.x-a.x)*i, a.y+(b.y-a.y)*i);
     }
 }
